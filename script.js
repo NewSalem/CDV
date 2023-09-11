fbb1f40a591e4ccbc63bc6855f08b7b994594c87
NewSalem <141784850+NewSalem@users.noreply.github.com>
2023-09-11 16:35:54 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Help prevent spam in email and apply email in browser //
-var name = "newjerusalem321";
-var domain = "gmail.com";
-// document.write ('<a href="' + 'mail' + 'to:'  + name + '@' + domain + '"> Email me </a>'); (to deny code for now ) //
